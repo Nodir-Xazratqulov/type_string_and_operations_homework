12e78defd9f7abac81a1d59d23b7ce165ea7076b
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-05-17 11:52:16 +0500

    str-3

--- String03.py
+++ String03.py
@@ -1,4 +1,4 @@
-def main():
+def main(a):
     """
     "*" Return a string that consists of only this character and is five in length.
     Args:
@@ -6,4 +6,5 @@ def main():
     Returns:
         str: return answer.
     """
-    return 
+    return str(a)
+print(main(5*'*'))
