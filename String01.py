1be37966ff1a076e09744c6c0f40674c7ee55249
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-05-17 11:49:18 +0500

    str-1

--- String01.py
+++ String01.py
@@ -1,8 +1,9 @@
-def main():
+def main(a):
     """"Return an optional string of length eight
     Args:
         None
     Returns:
         str: return answer.
     """
-    return 
+    return str(a)
+print(main(12))
